c5e32e86702a22e18a5dc31c9855e34fd98fd2c7
Dmitriy Beglov <ditransler@gmail.com>
2018-01-11 21:25:50 +0400

    Added Header on index and about pages

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const linkStyle = {
     marginRight: 15
-}
+};
 
 const Header = () => (
     <div>
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,8 @@
+import Header from '../components/Header';
+
 export default () => (
     <div>
+        <Header />
         <p>This is about page</p>
     </div>
 )
