71fa03eb7abe5e1600e75fc5d18603622d880c7c
Dmitriy Beglov <ditransler@gmail.com>
2018-01-14 20:18:09 +0400

    Added an example of a dynamic page (query string)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Layout from '../components/MyLayout';
+import Link from 'next/link';
+
+const PostLink = (props) => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+);
 
 export default () => (
-    <div>
-        <Layout>
-            <p>Hello, Next.js!</p>
-        </Layout>
-    </div>
+    <Layout>
+        <h1>My Blog</h1>
+        <ul>
+            <PostLink title="Hello Next.js" />
+            <PostLink title="Learn Next.js is awesome" />
+            <PostLink title="Deploy app with Zeit" />
+        </ul>
+    </Layout>
 )
