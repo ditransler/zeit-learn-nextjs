633f8584d62e620922af94945ee3e611398526b3
Dmitriy Beglov <ditransler@gmail.com>
2018-01-14 21:55:34 +0400

    Tried out fetch data for pages

--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,31 @@
 import Layout from '../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
 
-const Content = (props) => (
-    <div>
-        <h1>{props.url.query.title}</h1>
-        <p>This is the blog post content.</p>
-    </div>
-);
+const Content = (props) => {
+    const { show } = props;
+    return (
+        <div>
+            <h1>{show.name}</h1>
+            <p>{show.summary.replace(/<[/]?p>/g, '')}</p>
+            <img src={show.image.medium} />
+        </div>
+    );
+};
 
-export default (props) => (
+const Post = (props) => (
     <Layout>
-        <Content url={props.url} />
+        <Content show={props.show} />
     </Layout>
 );
+
+Post.getInitialProps = async function (context) {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+
+    console.log(`Fetched show: ${show.name}`);
+
+    return { show };
+}
+
+export default Post;
